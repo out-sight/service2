26320dfaac2db04cdc659d210a5ea2e8b0a76248
Vineesh_Devasia <vinihi83@gmail.com>
2017-04-27 15:07:25 +0530

    Added config for used config server with basic auth and added eureka client

--- outsight-service2/src/main/java/com/outsight/service2/OutsightService2Application.java
+++ outsight-service2/src/main/java/com/outsight/service2/OutsightService2Application.java
@@ -2,8 +2,10 @@ package com.outsight.service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OutsightService2Application {
 
 	public static void main(String[] args) {
